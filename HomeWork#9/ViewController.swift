605093ac322ffbc922c2d41e9ce1977dc780277f
Karabuzau <63594269+Karabuzau@users.noreply.github.com>
2023-01-28 22:16:21 +0600

    homework9

---
+++ HomeWork#9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork#9
+//
+//  Created by Бекжан on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+git 
+}
+
